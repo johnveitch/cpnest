b0213ea4982674233b8e5422eb3906f77f5164e0
John Veitch <john.veitch@ligo.org>
2016-11-21 23:10:01 +0000

    Add doc

--- tests/test_gaussian.py
+++ tests/test_gaussian.py
@@ -3,15 +3,18 @@ import numpy as np
 import cpnest
 
 class GaussianModel(object):
+    """
+    A simple gaussian model with parameters mean and sigma
+    """
     def __init__(self):
         pass
-    par_names=['mean2','sigma2']
+    par_names=['mean','sigma']
     bounds=[[-10,10],[0.01,1]]
     data = np.array([x for x in np.random.normal(0.5,0.5,size=1000)])
 
     @classmethod
     def log_likelihood(cls,x):
-        return -0.5*np.sum((cls.data-x.get('mean2'))**2/x.get('sigma2')**2) - len(cls.data)*np.log(x.get('sigma2')) - 0.5*np.log(2.0*np.pi)-1000
+        return -0.5*np.sum((cls.data-x.get('mean'))**2/x.get('sigma')**2) - len(cls.data)*np.log(x.get('sigma')) - 0.5*np.log(2.0*np.pi)-1000
     @staticmethod
     def log_prior(p):
         for i in range(p.dimension):
@@ -20,6 +23,9 @@ class GaussianModel(object):
 
 
 class GaussianTestCase(unittest.TestCase):
+    """
+    Test the gaussian model
+    """
     def setUp(self):
         self.work=cpnest.CPNest(GaussianModel,verbose=0,Nthreads=1)
 
