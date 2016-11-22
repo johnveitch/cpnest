099b84ae6597f6c0f69688cb2abfceb275baa49d
Walter Del Pozzo <walter.delpozzo@ligo.org>
2016-11-22 14:54:24 +0100

    added another prototype test

--- tests/test_gaussianmixture.py
+++ tests/test_gaussianmixture.py
@@ -1,26 +1,39 @@
 import unittest
 import numpy as np
 import cpnest
+from scipy.special import logsumexp
 
-class GaussianModel(object):
+class GaussianMixtureModel(object):
     """
     A simple gaussian model with parameters mean and sigma
     """
     def __init__(self):
         pass
-    par_names=['mean','sigma']
-    bounds=[[-10,10],[0.01,1]]
-    data = np.array([x for x in np.random.normal(0.5,0.5,size=1000)])
+    par_names=['mean1','sigma1','mean2','sigma2','weight']
+    bounds=[[-10,10],[0.01,1],[-10,10],[0.01,1],[0.0,1.0]]
+    data = []
+    for _ in range(1000):
+        if np.random.uniform(0.0,1.0) < 0.3:
+            data.append(np.random.normal(0.5,0.5))
+        else:
+            data.append(np.random.normal(-1.5,0.01))
 
     @classmethod
     def log_likelihood(cls,x):
-        return -0.5*np.sum((cls.data-x['mean'])**2/x['sigma']**2) - len(cls.data)*np.log(x['sigma']) - 0.5*np.log(2.0*np.pi)-1000
+        w = x('weight')
+        logL = 0.0
+        for d in data:
+            logL += 
+        
+        
+        
+        return -0.5*np.sum((cls.data-x('mean1'))**2/x('sigma1')**2) - len(cls.data)*np.log(x('sigma')) - 0.5*np.log(2.0*np.pi)-1000
 
     @staticmethod
     def log_prior(p):
         for i in range(p.dimension):
             if not p.parameters[i].inbounds(): return -np.inf
-        return -np.log(p('sigma'))
+        return -np.log(p('sigma1'))-np.log(p('sigma2'))
 
 
 class GaussianTestCase(unittest.TestCase):
@@ -40,4 +53,3 @@ def test_all():
 
 if __name__=='__main__':
     unittest.main(verbosity=2)
-
