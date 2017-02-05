Fix evidence calculation - fixes #9  - Reworked a lot of the nested sampler and MCMC code

cpnest/cpnest.py
@@ -39,13 +39,14 @@ class CPNest(object):
         for i in range(0,self.NUMBER_OF_PRODUCER_PROCESSES):
             p = mp.Process(target=self.Evolver.produce_sample, args=(self.ns_lock, self.queue, self.NS.jobID, self.NS.logLmin, self.seed+i, self.ip, self.port, self.authkey ))
             self.process_pool.append(p)
-        for i in range(0,self.NUMBER_OF_CONSUMER_PROCESSES):
-            p = mp.Process(target=self.NS.nested_sampling_loop, args=(self.sampler_lock, self.queue, self.port, self.authkey))
-            self.process_pool.append(p)
+        #for i in range(0,self.NUMBER_OF_CONSUMER_PROCESSES):
+        #    np = mp.Process(target=self.NS.nested_sampling_loop, args=(self.sampler_lock, self.queue, self.port, self.authkey))
+        #    self.process_pool.append(np)
         for each in self.process_pool:
             each.start()
+        self.NS.nested_sampling_loop(self.sampler_lock,self.queue,self.port,self.authkey)
         for each in self.process_pool:
-            each.join()
+            each.join(5)
 
         self.nested_samples = np.genfromtxt(self.NS.outfilename,names=True)
         self.posterior_samples = draw_posterior_many([self.nested_samples],[self.NS.Nlive],verbose=self.verbose)

cpnest/sampler.py
@@ -33,26 +33,34 @@ class Sampler(object):
         self.user = usermodel
         names = usermodel.par_names
         bounds = usermodel.bounds
-        self.cache = deque(maxlen=5*maxmcmc)
+        self.cache = deque(maxlen=maxmcmc)
         self.maxmcmc = maxmcmc
         self.Nmcmc = maxmcmc
         self.proposals = proposals.setup_proposals_cycle()
         self.poolsize = poolsize
-        self.evolution_points = [None]*self.poolsize
+        #self.evolution_points = [None]*self.poolsize
+        self.evolution_points = deque(maxlen=self.poolsize)
         self.verbose=verbose
         self.inParam = parameter.LivePoint(names,bounds)
         self.param = parameter.LivePoint(names,bounds)
         self.dimension = self.param.dimension
         for n in range(self.poolsize):
-            while True:
-                if self.verbose: sys.stderr.write("process {0!s} --> generating pool of {1:d} points for evolution --> {2:.3f} % complete\r".format(os.getpid(), self.poolsize, 100.0*float(n+1)/float(self.poolsize)))
-                self.evolution_points[n] = parameter.LivePoint(names,bounds)
-                self.evolution_points[n].initialise()
-                self.evolution_points[n].logP = self.user.log_prior(self.evolution_points[n])
-                if not(np.isinf(self.evolution_points[n].logP)): break
+          while True:
+            if self.verbose: sys.stderr.write("process {0!s} --> generating pool of {1:d} points for evolution --> {2:.3f} % complete\r".format(os.getpid(), self.poolsize, 100.0*float(n+1)/float(self.poolsize)))
+            p = parameter.LivePoint(names,bounds)
+            p.initialise()
+            p.logP = self.user.log_prior(p)
+            if np.isfinite(p.logP): break
+          p.logL=self.user.log_likelihood(p)
+          self.evolution_points.append(p)
         if self.verbose: sys.stderr.write("\n")
         self.kwargs = proposals.ProposalArguments(self.dimension)
-        self.kwargs.update(self.evolution_points)
+        self.kwargs.update(list(self.evolution_points))
+        for _ in range(self.poolsize):
+          s = self.evolution_points.popleft()
+          acceptance,jumps,s = self.metropolis_hastings(s,-np.inf,self.Nmcmc)
+          self.evolution_points.append(s)
+        self.kwargs.update(list(self.evolution_points))
 
     def produce_sample(self, consumer_lock, queue, IDcounter, logLmin, seed, ip, port, authkey):
         """
@@ -71,15 +79,15 @@ class Sampler(object):
             if logLmin.value==np.inf:
                 break
             acceptance,jumps,outParam = self.metropolis_hastings(self.inParam,logLmin.value,self.Nmcmc)
-            parameter.copy_live_point(self.evolution_points[np.random.randint(self.poolsize)],outParam)
-
+            #parameter.copy_live_point(self.evolution_points[np.random.randint(self.poolsize)],outParam)
+            self.evolution_points.append(outParam.copy())
             queue.put((id,acceptance,jumps,np.array([outParam[n] for n in outParam.names]),outParam.logP,outParam.logL))
             if self.counter == 0 and len(self.cache)==5*self.maxmcmc:
                 self.autocorrelation()
-                self.kwargs.update(self.evolution_points)
-            elif (self.counter%(self.poolsize/4))==0:
+                self.kwargs.update(list(self.evolution_points))
+            elif (self.counter%(self.poolsize/2))==0:
                 self.autocorrelation()
-                self.kwargs.update(self.evolution_points)
+                self.kwargs.update(list(self.evolution_points))
             self.counter += 1
         sys.stderr.write("Sampler process {0!s}, exiting\n".format(os.getpid()))
         return 0
@@ -91,29 +99,28 @@ class Sampler(object):
         accepted = 0
         rejected = 1
         jumps = 0
-        parameter.copy_live_point(self.param,inParam)
-        logp_old = self.user.log_prior(self.param)
+        oldparam=inParam.copy()
+        #parameter.copy_live_point(self.param,inParam)
+        logp_old = self.user.log_prior(oldparam)
         while (jumps < nsteps or accepted==0):
-            self.param,log_acceptance = self.proposals[jumps%100].get_sample(self.param,self.evolution_points,self.kwargs)
-            self.param.logP = self.user.log_prior(self.param)
-            if self.param.logP-logp_old > log_acceptance:
-                self.param.logL = self.user.log_likelihood(self.param)
-                if self.param.logL > logLmin:
-                    parameter.copy_live_point(inParam,self.param)
-                    logp_old = self.param.logP
-                    accepted+=1
+            newparam,log_acceptance = self.proposals[jumps%100].get_sample(oldparam.copy(),list(self.evolution_points),self.kwargs)
+            newparam.logP = self.user.log_prior(newparam)
+            if newparam.logP-logp_old > log_acceptance:
+                newparam.logL = self.user.log_likelihood(newparam)
+                if newparam.logL > logLmin:
+                  oldparam=newparam.copy()
+                  logp_old = newparam.logP
+                  accepted+=1
                 else:
-                    parameter.copy_live_point(self.param,inParam)
-                    rejected+=1
+                  rejected+=1
             else:
-                parameter.copy_live_point(self.param,inParam)
                 rejected+=1
-                self.cache.append(np.array(([x.value for x in inParam.parameters])))
+
+            self.cache.append(np.array(([x.value for x in oldparam.parameters])))
             jumps+=1
             if jumps==10*self.maxmcmc:
-                return (0.0,jumps,inParam)
-#            exit()
-        return (float(accepted)/float(rejected+accepted),jumps,inParam)
+              print('Warning, MCMC chain exceeded {0} iterations!'.format(10*self.maxmcmc))
+        return (float(accepted)/float(rejected+accepted),jumps,oldparam)
 
     def autocorrelation(self):
         """

tests/test_1d.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+from scipy import integrate
 import cpnest
 
 class GaussianModel(object):
@@ -9,12 +10,12 @@ class GaussianModel(object):
     def __init__(self):
         pass
     par_names=['x']
-    bounds=[[-100,100]]
-    analytic_log_Z=0.0
+    bounds=[[-10,10]]
+    analytic_log_Z=0.0 - np.log(bounds[0][1] - bounds[0][0])
 
     @classmethod
     def log_likelihood(cls,p):
-        return -0.5*(p['x']**2) - np.log(2.0*np.pi)
+        return -0.5*(p['x']**2) - 0.5*np.log(2.0*np.pi)
 
     @staticmethod
     def log_prior(p):
@@ -28,12 +29,13 @@ class GaussianTestCase(unittest.TestCase):
     Test the gaussian model
     """
     def setUp(self):
-        self.work=cpnest.CPNest(GaussianModel,verbose=1,Nthreads=1,Nlive=100,maxmcmc=100)
+        self.work=cpnest.CPNest(GaussianModel,verbose=2,Nthreads=1,Nlive=50,maxmcmc=1000)
 
     def test_run(self):
         self.work.run()
-        tolerance = 0.5
-        self.assertTrue(np.abs(self.work.NS.logZ - GaussianModel.analytic_log_Z)<tolerance, 'Incorrect evidence for normalised distribution: {0}'.format(self.work.NS.logZ ))
+        tolerance = np.sqrt(self.work.NS.state.info/self.work.NS.Nlive)
+        print('Tolerance: {0:0.3f}'.format(tolerance))
+        self.assertTrue(np.abs(self.work.NS.logZ - GaussianModel.analytic_log_Z)<tolerance, 'Incorrect evidence for normalised distribution: {0:.3f} instead of {1:.3f}'.format(self.work.NS.logZ,GaussianModel.analytic_log_Z ))
 
 
 