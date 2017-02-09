f67db5eff64a50da37c7a2b4a46acddd8634b0a8
John Veitch <john.veitch@ligo.org>
2017-02-09 15:37:04 +0000

    Add check on posterior distribution from 1d case

--- tests/test_1d.py
+++ tests/test_1d.py
@@ -1,21 +1,24 @@
 import unittest
 import numpy as np
-from scipy import integrate
+from scipy import integrate,stats
 import cpnest.model
+import matplotlib as mpl
+mpl.use('Agg')
+from matplotlib import pyplot as plt
 
 class GaussianModel(cpnest.model.Model):
     """
     A simple gaussian model with parameters mean and sigma
     """
     def __init__(self):
-        pass
+        self.distr = stats.norm(loc=0,scale=1.0)
     names=['x']
     bounds=[[-10,10]]
     analytic_log_Z=0.0 - np.log(bounds[0][1] - bounds[0][0])
 
-    @classmethod
-    def log_likelihood(cls,p):
-        return -0.5*(p['x']**2) - 0.5*np.log(2.0*np.pi)
+    def log_likelihood(self,p):
+        return self.distr.logpdf(p['x'])
+        #return -0.5*(p['x']**2) - 0.5*np.log(2.0*np.pi)
 
 
 class GaussianTestCase(unittest.TestCase):
@@ -23,14 +26,24 @@ class GaussianTestCase(unittest.TestCase):
     Test the gaussian model
     """
     def setUp(self):
-        self.work=cpnest.CPNest(GaussianModel(),verbose=2,Nthreads=1,Nlive=50,maxmcmc=1000)
-
-    def test_run(self):
+        self.model=GaussianModel()
+        self.work=cpnest.CPNest(self.model,verbose=2,Nthreads=1,Nlive=500,maxmcmc=5000)
         self.work.run()
+
+    def test_evidence(self):
         # 2 sigma tolerance
         tolerance = 2.0*np.sqrt(self.work.NS.state.info/self.work.NS.Nlive)
         print('Tolerance: {0:0.3f}'.format(tolerance))
         self.assertTrue(np.abs(self.work.NS.logZ - GaussianModel.analytic_log_Z)<tolerance, 'Incorrect evidence for normalised distribution: {0:.3f} instead of {1:.3f}'.format(self.work.NS.logZ,GaussianModel.analytic_log_Z ))
+        
+    def test_posterior_ks(self):
+        pos=self.work.posterior_samples['x']
+        t,pval=stats.kstest(pos,self.model.distr.cdf)
+        plt.figure()
+        plt.hist(pos.ravel(),normed=True)
+        plt.title('KS test = %f'%(pval))
+        plt.savefig('posterior.png')
+        self.assertTrue(pval>0.01,'KS test failed: KS stat = {0}'.format(pval))
 
 
 
