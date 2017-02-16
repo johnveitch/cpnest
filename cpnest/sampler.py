253b33360ab9f8a22c3cd2fa8be2cb63c31c3ebb
John Veitch <john.veitch@ligo.org>
2017-02-16 23:54:33 +0000

    Add Will's ACL estimator

--- cpnest/sampler.py
+++ cpnest/sampler.py
@@ -35,6 +35,7 @@ class Sampler(object):
         self.user = usermodel
         self.maxmcmc = maxmcmc
         self.Nmcmc = maxmcmc
+        self.Nmcmc_exact = float(maxmcmc)
         self.cache = deque(maxlen=maxmcmc)
         self.proposals = proposal.DefaultProposalCycle()
         self.poolsize = poolsize
@@ -43,6 +44,7 @@ class Sampler(object):
         self.inParam = parameter.LivePoint(self.user.names)
         self.param = parameter.LivePoint(self.user.names)
         self.dimension = self.param.dimension
+        self.acceptance=0.0
         self.initialised=False
         
     def reset(self):
@@ -62,6 +64,21 @@ class Sampler(object):
           self.evolution_points.append(s)
         self.proposals.set_ensemble(self.evolution_points)
         self.initialised=True
+
+    def estimate_nmcmc(self, safety=5, tau = 1000):
+        """
+        Estimate autocorrelation length of chain using acceptance fraction
+        ACL = (2/acc) - 1
+        multiplied by a safety margin of 5
+        Uses moving average with decay time tau iterations
+        Taken from W. Farr's github.com/farr/Ensemble.jl
+        """
+        if self.acceptance==0:
+            self.Nmcmc_exact = (1.0 + 1.0/tau)*self.Nmcmc_exact
+        else:
+            self.Nmcmc_exact = (1.0 - 1.0/tau)*self.Nmcmc_exact + (safety/tau)*(2.0/self.acceptance - 1.0)
+        self.Nmcmc = max(safety,min(self.maxmcmc, int(round(self.Nmcmc_exact))))
+        return self.Nmcmc
 
     def produce_sample(self, consumer_lock, queue, IDcounter, logLmin, seed, ip, port, authkey):
         """
@@ -84,11 +101,8 @@ class Sampler(object):
             acceptance,jumps,outParam = self.metropolis_hastings(self.inParam,logLmin.value,self.Nmcmc)
             self.evolution_points.append(outParam.copy())
             queue.put((id,acceptance,jumps,outParam))
-            if self.counter == 0 and len(self.cache)==5*self.maxmcmc:
-                self.autocorrelation()
-                self.proposals.set_ensemble(self.evolution_points)
-            elif (self.counter%(self.poolsize/2))==0 or acceptance<0.01:
-                self.autocorrelation()
+            if (self.counter%(self.poolsize/10))==0 or self.acceptance<0.01:
+                #self.autocorrelation()
                 self.proposals.set_ensemble(self.evolution_points)
             self.counter += 1
         sys.stderr.write("Sampler process {0!s}, exiting\n".format(os.getpid()))
@@ -99,7 +113,7 @@ class Sampler(object):
         metropolis-hastings loop to generate the new live point taking nmcmc steps
         """
         accepted = 0
-        rejected = 1
+        rejected = 0
         jumps = 0
         oldparam=inParam
         logp_old = self.user.log_prior(oldparam)
@@ -121,6 +135,8 @@ class Sampler(object):
             jumps+=1
             if jumps==10*self.maxmcmc:
               print('Warning, MCMC chain exceeded {0} iterations!'.format(10*self.maxmcmc))
+        self.acceptance = float(accepted)/float(jumps)
+        self.estimate_nmcmc()
         return (float(accepted)/float(rejected+accepted),jumps,oldparam)
 
     def autocorrelation(self):
