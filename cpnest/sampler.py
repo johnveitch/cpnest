f4ff31137a06c3850d5ef47efce9a0903a714b44
Walter Del Pozzo <walter.delpozzo@ligo.org>
2017-11-21 12:44:00 +0100

    changed the phylosopy of the sampler

--- cpnest/sampler.py
+++ cpnest/sampler.py
@@ -35,21 +35,25 @@ class Sampler(object):
     JumpProposal class to use (defaults to proposals.DefaultProposalCycle)
     
     """
-    def __init__(self,usermodel,maxmcmc,seed=None, verbose=False,poolsize=1000, proposal=None):
+    def __init__(self,usermodel,maxmcmc,seed=None, verbose=False, poolsize=1000, proposal=None):
         self.user = usermodel
+        self.initial_mcmc = maxmcmc//2
         self.maxmcmc = maxmcmc
-        self.Nmcmc = maxmcmc
-        self.seed=seed
-        self.Nmcmc_exact = float(maxmcmc)
+
         if proposal is None:
             self.proposal = DefaultProposalCycle()
         else:
             self.proposal = proposal
+
+        self.Nmcmc = self.initial_mcmc
+        self.Nmcmc_exact = float(self.initial_mcmc)
+
         self.poolsize = poolsize
-        self.evolution_points = deque(maxlen=self.poolsize + 1) # +1 for the point to evolve
+        self.evolution_points = deque(maxlen=self.poolsize)
         self.verbose=verbose
         self.acceptance=0.0
         self.initialised=False
+        self.samples = [] # the list of samples from the mcmc chain
         
     def reset(self):
         """
@@ -65,15 +69,18 @@ class Sampler(object):
           p.logL=self.user.log_likelihood(p)
           self.evolution_points.append(p)
         if self.verbose > 2: sys.stderr.write("\n")
-        self.proposal.set_ensemble(self.evolution_points)
-        for _ in range(len(self.evolution_points)):
-          s = self.evolution_points.popleft()
-          s = self.metropolis_hastings(s,-np.inf)
-          self.evolution_points.append(s)
-        self.proposal.set_ensemble(self.evolution_points)
+
+        self.proposals.set_ensemble(self.evolution_points)
+
+        self.metropolis_hastings(-np.inf)
+        
+        if self.verbose > 2: sys.stderr.write("\n")
+        if self.verbose > 2: sys.stderr.write("Initial estimated ACL = {0:d}\n".format(self.Nmcmc))
+        self.proposals.set_ensemble(self.evolution_points)
+
         self.initialised=True
 
-    def estimate_nmcmc(self, safety=5, tau=None):
+    def estimate_nmcmc(self, safety=1, tau=None):
         """
         Estimate autocorrelation length of chain using acceptance fraction
         ACL = (2/acc) - 1
@@ -90,6 +97,7 @@ class Sampler(object):
         
         self.Nmcmc_exact = float(min(self.Nmcmc_exact,self.maxmcmc))
         self.Nmcmc = max(safety,int(self.Nmcmc_exact))
+
         return self.Nmcmc
 
     def produce_sample(self, queue, logLmin ):
@@ -105,48 +113,46 @@ class Sampler(object):
         while True:
             if logLmin.value==np.inf:
                 break
-            # Pick a random point from the ensemble to start with
-            # Pop it out the stack to prevent cloning
-            #param = self.evolution_points.rotate()
+        
+            self.metropolis_hastings(logLmin.value)
+            outParam = self.evolution_points[np.random.randint(self.poolsize)]
 
-#            param = self.evolution_points[np.random.randint(self.poolsize)]
-#            self.evolution_points.remove(param)
-            param = self.evolution_points.popleft()
-            
-            outParam = self.metropolis_hastings(param,logLmin.value)
-           
-            # Put sample back in the stack
-            self.evolution_points.append(outParam.copy())
-            # If we bailed out then flag point as unusable
-            if self.acceptance==0.0:
-                outParam.logL=-np.inf
             # Push the sample onto the queue
             queue.put((self.acceptance,self.Nmcmc,outParam))
             # Update the ensemble every now and again
             if (self.counter%(self.poolsize/10))==0 or self.acceptance<0.001:
                 self.proposal.set_ensemble(self.evolution_points)
             self.counter += 1
+
+        print "MCMC samples accumulated = ",len(self.samples)
+        import numpy.lib.recfunctions as rfn
+        self.mcmc_samples = rfn.stack_arrays([self.samples[j].asnparray() for j in range(len(self.samples))],usemask=False)
+        np.savetxt(os.path.join('mcmc_chain_%s.dat'%os.getpid()),self.mcmc_samples.ravel(),header=' '.join(self.mcmc_samples.dtype.names),newline='\n',delimiter=' ')
         sys.stderr.write("Sampler process {0!s}, exiting\n".format(os.getpid()))
         return 0
 
-    def metropolis_hastings(self,inParam,logLmin):
+    def metropolis_hastings(self,logLmin):
         """
         metropolis-hastings loop to generate the new live point taking nmcmc steps
         """
         accepted = 0
-        oldparam = inParam.copy()
-        logp_old = self.user.log_prior(oldparam)
+       
         for jumps in range(self.Nmcmc):
-            newparam = self.proposal.get_sample(oldparam.copy())
-            newparam.logP = self.user.log_prior(newparam)
-            if newparam.logP-logp_old + self.proposal.log_J > log(random()):
-                newparam.logL = self.user.log_likelihood(newparam)
-                if newparam.logL > logLmin:
-                  oldparam = newparam
-                  logp_old = newparam.logP
-                  accepted+=1
 
-        self.acceptance = float(accepted)/float(self.Nmcmc)
+            for j in range(self.poolsize):
+
+                oldparam = self.evolution_points.popleft()
+                logp_old = self.user.log_prior(oldparam)
+                newparam = self.proposals.get_sample(oldparam.copy())
+                newparam.logP = self.user.log_prior(newparam)
+                if newparam.logP-logp_old + self.proposals.log_J > log(random()):
+                    newparam.logL = self.user.log_likelihood(newparam)
+                    if newparam.logL > logLmin:
+                      oldparam = newparam
+                      logp_old = newparam.logP
+                      accepted+=1
+                # Put sample back in the stack
+                self.samples.append(oldparam)
+                self.evolution_points.append(oldparam)
+        self.acceptance = float(accepted)/float(self.Nmcmc*self.poolsize)
         self.estimate_nmcmc()
-        return oldparam
-
