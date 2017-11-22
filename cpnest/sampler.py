50f48ff7f1f996696d9ff643199f4639413e4c89
Walter Del Pozzo <walter.delpozzo@ligo.org>
2017-11-22 20:36:04 +0100

    deugging

--- cpnest/sampler.py
+++ cpnest/sampler.py
@@ -158,7 +158,6 @@ class Sampler(object):
             sub_accepted = 0
             oldparam = self.evolution_points.popleft()
             logp_old = self.user.log_prior(oldparam)
-
             while True:
                 sub_counter += 1
                 newparam = self.proposals.get_sample(oldparam.copy())
@@ -173,6 +172,7 @@ class Sampler(object):
                     break
             if queue is not None and oldparam.logL > logLmin: queue.put((float(sub_accepted)/float(sub_counter),sub_counter,oldparam))
             counter += sub_counter
+
             # Put sample back in the stack
             self.samples.append(oldparam)
             self.evolution_points.append(oldparam)
