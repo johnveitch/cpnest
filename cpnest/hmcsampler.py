985bde8de113ed83bf5da591b7a7536043d9a470
Walter Del Pozzo <walter.delpozzo@ligo.org>
2017-12-22 18:12:50 +0100

    suclassing the sampler class

--- cpnest/hmcsampler.py
+++ cpnest/hmcsampler.py
@@ -11,8 +11,9 @@ from scipy.stats import multivariate_normal
 
 from . import parameter
 from . import proposal
+from sampler import Sampler
 
-class HMCSampler(object):
+class HMCSampler(Sampler):
     """
     Hamiltonian Monte Carlo Sampler class.
     Initialisation arguments:
@@ -31,17 +32,9 @@ class HMCSampler(object):
     number of objects for the gradients estimation
     default: 1000
     """
-    def __init__(self,usermodel, maxmcmc, verbose=False, poolsize=1000, l=1.0):
-        self.user           = usermodel
-        self.maxmcmc        = maxmcmc
-        self.Nmcmc          = maxmcmc
-        self.Nmcmc_exact    = float(maxmcmc)
-        self.proposals      = proposal.DefaultProposalCycle()
-        self.poolsize       = poolsize
-        self.positions      = deque(maxlen=self.poolsize + 1) # +1 for the point to evolve
-        self.verbose        = verbose
-        self.acceptance     = 0.0
-        self.initialised    = False
+    def __init__(self, *args, l=1.0, **kwargs):
+        super(HMCSampler,self).__init__(*args, **kwargs)
+        self.step = self.constrained_leapfrog_step
         self.gradients      = {}
         # step size choice from http://www.homepages.ucl.ac.uk/~ucakabe/papers/Bernoulli_11b.pdf
         # which asks for the relation step_size = l * dims**(1/4)
@@ -55,27 +48,8 @@ class HMCSampler(object):
         """
         Initialise the sampler
         """
-        for n in range(self.poolsize):
-            while True:
-                if self.verbose > 2: sys.stderr.write("process {0!s} --> generating pool of {1:d} points for evolution --> {2:.0f} % complete\r".format(os.getpid(), self.poolsize, 100.0*float(n+1)/float(self.poolsize)))
-                p = self.user.new_point()
-                p.logP = self.user.log_prior(p)
-                if np.isfinite(p.logP): break
-            p.logL=self.user.log_likelihood(p)
-            self.positions.append(p)
-        if self.verbose > 2: sys.stderr.write("\n")
+        super(HMCSampler, self).reset()
 
-        self.proposals.set_ensemble(self.positions)
-
-        # seed the chains with a standard MCMC
-        for j in range(len(self.positions)):
-            if self.verbose > 2: sys.stderr.write("process {0!s} --> initial MCMC evolution for {1:d} points --> {2:.0f} % complete\r".format(os.getpid(), self.poolsize, 100.0*float(j+1)/float(self.poolsize)))
-            s = self.positions.popleft()
-            s = self.metropolis_hastings(s,-np.inf)
-            self.positions.append(s)
-        if self.verbose > 2: sys.stderr.write("\n")
-
-        self.proposals.set_ensemble(self.positions)
         self.momenta_distribution = multivariate_normal(cov=self.proposals.mass_matrix)
         self.step_size = self.l * float(len(self.positions[0].names))**(0.25)
 
@@ -88,69 +62,6 @@ class HMCSampler(object):
 
         if self.verbose > 2: sys.stderr.write("done\n")
         self.initialised=True
-
-    def estimate_nmcmc(self, safety=1, tau=None):
-        """
-        Estimate autocorrelation length of chain using acceptance fraction
-        ACL = (2/acc) - 1
-        multiplied by a safety margin of 5
-        Uses moving average with decay time tau iterations (default: self.poolsize)
-        Taken from W. Farr's github.com/farr/Ensemble.jl
-        """
-        if tau is None: tau = self.poolsize
-
-        if self.acceptance == 0.0:
-            self.Nmcmc_exact = (1.0 + 1.0/tau)*self.Nmcmc_exact
-#            print self.Nmcmc_exact,(1.0 - 1.0/tau)
-        else:
-            self.Nmcmc_exact = (1.0 - 1.0/tau)*self.Nmcmc_exact + (safety/tau)*(2.0/self.acceptance - 1.0)
-#            print self.Nmcmc_exact,(1.0 - 1.0/tau),(safety/tau),(2.0/self.acceptance - 1.0),(safety/tau)*(2.0/self.acceptance - 1.0)
-
-        self.Nmcmc_exact = float(min(self.Nmcmc_exact,self.maxmcmc))
-        self.Nmcmc       = max(safety,int(self.Nmcmc_exact))
-
-        return self.Nmcmc
-
-    def produce_sample(self, queue, logLmin, seed, ip, port, authkey):
-        """
-        main loop that generates samples and puts them in the queue for the nested sampler object
-        """
-        if not self.initialised:
-            self.reset()
-        # Prevent process from zombification if consumer thread exits
-        queue.cancel_join_thread()
-        self.seed = seed
-        np.random.seed(seed=self.seed)
-        unused = 0
-        while(1):
-            if logLmin.value==np.inf:
-                break
-            # Pick a point from the ensemble to start with
-            position = self.positions.popleft()
-            # evolve it according to hamilton equations
-            newposition = self.hamiltonian_sampling(position,logLmin.value)
-            # Put sample back in the stack
-            self.positions.append(newposition.copy())
-
-            # If we bailed out then flag point as unusable
-            if self.acceptance==0.0:
-                newposition.logL=-np.inf
-            # Push the sample onto the queue
-            queue.put((self.acceptance,self.jumps,newposition))
-            
-            # Update the ensemble every now and again
-            if (self.counter%(self.poolsize/10))==0:
-                self.proposals.set_ensemble(self.positions)
-                # update the gradients
-                logProbs = np.array([-p.logP for p in self.positions])
-                self.estimate_gradient(logProbs, 'logprior')
-                logProbs = np.array([-p.logL for p in self.positions])
-                self.estimate_gradient(logProbs, 'loglikelihood')
-                self.momenta_distribution = multivariate_normal(cov=self.proposals.mass_matrix)
-
-            self.counter += 1
-        sys.stderr.write("Sampler process {0!s}, exiting\n".format(os.getpid()))
-        return 0
     
     def estimate_gradient(self, logProbs, type):
         
@@ -305,75 +216,6 @@ class HMCSampler(object):
             momentum[k] += - 0.5 * self.step_size * g[j]
 
         return position, momentum
-    
-    def hamiltonian_sampling(self, initial_position, logLmin):
-        """
-        hamiltonian sampling loop to generate
-        the new live point taking nmcmc steps
-        """
-        self.jumps  = 0
-        accepted    = 0
-        oldparam    = initial_position.copy()
-        
-        while self.jumps < self.Nmcmc:
-            
-            # generate the initial momentum from its canonical distribution
-            v                = np.atleast_1d(self.momenta_distribution.rvs())
-            initial_momentum = self.user.new_point()
-            
-            for j,k in enumerate(self.positions[0].names):
-                initial_momentum[k] = v[j]
-
-            starting_energy       = self.hamiltonian(oldparam, initial_momentum)
-            newparam, newmomentum = self.constrained_leapfrog_step(oldparam.copy(), initial_momentum.copy(), logLmin)
-            current_energy        = self.hamiltonian(newparam, newmomentum)
-
-            logp_accept = min(0.0, starting_energy - current_energy)
-    
-            if logp_accept > np.log(random()):
-                if newparam.logL > logLmin:
-                    oldparam        = newparam
-                    accepted       += 1
-            
-            self.jumps+=1
-            if self.jumps > self.maxmcmc: break
-
-        self.acceptance = float(accepted)/float(self.jumps)
-        self.estimate_nmcmc()
-#        self.autotune()
-        return oldparam
-
-    def metropolis_hastings(self, inParam, logLmin):
-        """
-        metropolis-hastings loop to generate
-        the new live point taking nmcmc steps
-        """
-        self.jumps  = 0
-        accepted    = 0
-        oldparam    = inParam.copy()
-        logp_old    = self.user.log_prior(oldparam)
-        
-        while self.jumps < self.Nmcmc:
-            
-            newparam        = self.proposals.get_sample(oldparam.copy())
-            newparam.logP   = self.user.log_prior(newparam)
-            
-            if newparam.logP-logp_old + self.proposals.log_J > log(random()):
-                newparam.logL = self.user.log_likelihood(newparam)
-                
-                if newparam.logL > logLmin:
-                    oldparam = newparam
-                    logp_old = newparam.logP
-                    accepted+=1
-            
-            self.jumps+=1
-            
-            if self.jumps > self.maxmcmc: break
-
-        self.acceptance = float(accepted)/float(self.jumps)
-        self.estimate_nmcmc()
-        
-        return oldparam
     
     def autotune(self, target = 0.654):
         if self.acceptance < target: self.steps -= 1
