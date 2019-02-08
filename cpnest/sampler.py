35206ed06ae0f4a5cea6412ef6631e4cb8dab693
Walter Del Pozzo <walter.delpozzo@ligo.org>
2019-02-08 13:07:12 +0100

    removed the innjection routine

--- cpnest/sampler.py
+++ cpnest/sampler.py
@@ -300,13 +300,6 @@ class HamiltonianMonteCarloSampler(Sampler):
                 if self.proposal.log_J > np.log(random()):
                     oldparam        = newparam
                     sub_accepted   += 1
-            
-                # if we are stuck in some low likelihood region and we are never accepting,
-                # inject a new point in the pool starting from the existing ones
-                if sub_counter >= self.maxmcmc:
-                    oldparam = self.inject_sample()
-                    self.evolution_points.append(oldparam)
-                    break
                 
                 self.evolution_points.append(oldparam)
 
