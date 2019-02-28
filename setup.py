9825bde9d6331788d117379a8b16b6ae41ea3bef
Walter Del Pozzo <walter.delpozzo@ligo.org>
2019-02-28 13:54:05 +0100

    fixed an incompatibility issue with anaconda and numpy

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # Always prefer setuptools over distutils
+import numpy
 from setuptools import setup, find_packages
 from setuptools import Extension
 from setuptools.command.build_ext import build_ext as _build_ext
@@ -14,7 +15,6 @@ class build_ext(_build_ext):
         _build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
         __builtins__.__NUMPY_SETUP__ = False
-        import numpy
         self.include_dirs.append(numpy.get_include())
 
 
