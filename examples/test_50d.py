a14f1abb20e76166964bca1320026d076e54cf98
Walter Del Pozzo <walter.delpozzo@ligo.org>
2020-02-23 20:18:43 +0100

    removed a case in which the sampler would get stuck in an infinite loop. added also helper functions

--- examples/test_50d.py
+++ examples/test_50d.py
@@ -33,8 +33,8 @@ class GaussianTestCase(unittest.TestCase):
     Test the gaussian model
     """
     def setUp(self):
-        self.model=GaussianModel(dim = 50)
-        self.work=cpnest.CPNest(self.model, verbose=2, nthreads=1, nlive=1000, maxmcmc=5000, poolsize=1000)
+        self.model=GaussianModel(dim = 20)
+        self.work=cpnest.CPNest(self.model, verbose=2, nthreads=4, nlive=1000, maxmcmc=5000, poolsize=32, nslice=4)
 
     def test_run(self):
         self.work.run()
