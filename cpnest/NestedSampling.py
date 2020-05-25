584a03e2acddfb416ffd4698284cd355eec864c8
Walter Del Pozzo <walter.delpozzo@ligo.org>
2020-05-25 09:18:07 +0200

    added the information  to the final output

--- cpnest/NestedSampling.py
+++ cpnest/NestedSampling.py
@@ -231,7 +231,8 @@ class NestedSampler(object):
         Write the evidence logZ and maximum likelihood to the evidence_file
         """
         with open(self.evidence_file,"w") as f:
-            f.write('{0:.5f} {1:.5f}\n'.format(self.state.logZ, self.logLmax.value))
+            f.write('#logZ\tlogLmax\tH\n')
+            f.write('{0:.5f} {1:.5f} {2:.2f\n'.format(self.state.logZ, self.logLmax.value, self.state.info))
 
     def setup_random_seed(self,seed):
         """
