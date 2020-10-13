c1159b2f9b42a6f4ef4513a07942fcfffb5aef7b
John Veitch <john.veitch@ligo.org>
2020-10-13 16:58:02 +0100

    Get version number in module from distribution

--- cpnest/__init__.py
+++ cpnest/__init__.py
@@ -2,9 +2,15 @@ import logging
 from .logger import CPNestLogger
 from .cpnest import CPNest
 
-logging.setLoggerClass(CPNestLogger)
+# Get the version number from git tag
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "dev"
 
-__version__ = '0.9.10'
+logging.setLoggerClass(CPNestLogger)
 
 __all__ = ['model',
            'NestedSampling',
