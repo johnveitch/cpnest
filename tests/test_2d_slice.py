793f5ec8bd2d2a7a96a083d13dea143d9d9600d2
John Veitch <john.veitch@ligo.org>
2021-01-08 15:37:09 +0000

    Add tests for slice sampler

---
+++ tests/test_2d_slice.py
@@ -0,0 +1,43 @@
+import unittest
+import numpy as np
+import cpnest.model
+
+class GaussianModel(cpnest.model.Model):
+    """
+    A simple 2 dimensional gaussian
+    """
+    def __init__(self):
+        pass
+    names=['x','y']
+    bounds=[[-10,10],[-10,10]]
+    analytic_log_Z=0.0 - np.log(bounds[0][1]-bounds[0][0]) - np.log(bounds[1][1]-bounds[1][0])
+
+    def log_likelihood(self,p):
+        return -0.5*(p['x']**2 + p['y']**2) - np.log(2.0*np.pi)
+
+    def log_prior(self,p):
+        return super(GaussianModel,self).log_prior(p)
+
+    def force(self, p):
+        f = np.zeros(1, dtype = {'names':p.names, 'formats':['f8' for _ in p.names]})
+        return f
+
+class GaussianTestCase(unittest.TestCase):
+    """
+    Test the gaussian model
+    """
+    def setUp(self):
+        self.work=cpnest.CPNest(GaussianModel(),verbose=1,nthreads=1,nlive=1000,maxmcmc=5000,poolsize=1000,nslice=1)
+
+    def test_run(self):
+        self.work.run()
+        # 2 sigma tolerance
+        tolerance = 2.0*np.sqrt(self.work.NS.state.info/self.work.NS.Nlive)
+        self.assertTrue(np.abs(self.work.NS.logZ - GaussianModel.analytic_log_Z)<tolerance, 'Incorrect evidence for normalised distribution: {0} +/- {2} instead of {1}'.format(self.work.NS.logZ ,GaussianModel.analytic_log_Z, tolerance))
+
+def test_all():
+    unittest.main(verbosity=2)
+
+if __name__=='__main__':
+    unittest.main(verbosity=2)
+
---
+++ tests/test_ring_slice.py
@@ -0,0 +1,48 @@
+import unittest
+import numpy as np
+import cpnest.model
+
+class RingModel(cpnest.model.Model):
+    """
+    A circular ring in parameter space
+    logZ ~ -2.31836
+    """
+    names=['x','y']
+    bounds=[[-2,2],[-2,2]]
+    data = None
+    analytic_log_Z = -2.31836
+    @staticmethod
+    def log_likelihood(x):
+        return log_ring(x['x'],x['y'])
+    def force(self, p):
+        f = np.zeros(1, dtype = {'names':p.names, 'formats':['f8' for _ in p.names]})
+        return f
+
+def log_ring(x, y, radius=1.0, thickness=0.1):
+    r = np.sqrt(x**2+y**2)
+    return -0.5*(radius-r)**2/thickness**2
+
+class RingTestCase(unittest.TestCase):
+    """
+    Test the gaussian model
+    """
+    def setUp(self):
+        self.model=RingModel()
+        self.work=cpnest.CPNest(self.model,verbose=1,nthreads=4,nslice=4,nlive=1000,maxmcmc=1000)
+        self.work.run()
+
+
+    def test_evidence(self):
+        # 2 sigma tolerance
+        tolerance = 2.0*np.sqrt(self.work.NS.state.info/self.work.NS.Nlive)
+        print('2-sigma statistic error in logZ: {0:0.3f}'.format(tolerance))
+        print('Analytic logZ {0}'.format(self.model.analytic_log_Z))
+        print('Estimated logZ {0}'.format(self.work.NS.logZ))
+        self.assertTrue(np.abs(self.work.NS.logZ - RingModel.analytic_log_Z)<tolerance, 'Incorrect evidence for normalised distribution: {0:.3f} +/ {2:.3f} instead of {1:.3f}'.format(self.work.NS.logZ,RingModel.analytic_log_Z,tolerance ))
+
+def test_all():
+    unittest.main(verbosity=2)
+
+if __name__=='__main__':
+        unittest.main(verbosity=0)
+
