8e34cc84b9d0f04befda18234138eea6cde0246e
alexlyttle <a.j.lyttle@outlook.com>
2021-01-17 16:50:46 +0000

    Added context manager and handler classes

--- cpnest/utils.py
+++ cpnest/utils.py
@@ -2,29 +2,88 @@ import os
 import logging
 
 # Default formats and level names
-FMT = '%(asctime)s - %(name)-8s: %(message)s'
-DATE_FMT = '%Y-%m-%d, %H:%M:%S'
+FORMATTER = logging.Formatter('%(asctime)s - %(name)-8s: %(message)s', datefmt='%Y-%m-%d, %H:%M:%S')
 LEVELS = ['CRITICAL', 'WARNING', 'INFO', 'DEBUG']
+LOGGER = logging.getLogger('cpnest.utils')
 
-def file_handler(fname, path=''):
-    """
-    Make a file handler with consistent formatting
 
-    fname : str
-        Filename of log file
+class Handler(logging.Handler):
 
-    path : str, optional
-        Path to save the file. Defaults to current working directory
+    def __init__(self, verbose=0, **kwargs):
+        super().__init__(**kwargs)
+        self.set_verbosity(verbose)
+        self.setFormatter(FORMATTER)
     
-    """
-    fh = logging.FileHandler(os.path.join(path, fname))
-    fh.setFormatter(logging.Formatter(FMT, datefmt=DATE_FMT))
-    return fh
+    def get_verbosity(self):
+        return self._verbose
 
-def stream_handler():
+    def set_verbosity(self, verbose):
+        self._verbose = verbose
+        self.setLevel(LEVELS[verbose])
+        
+
+class ConsoleHandler(Handler, logging.StreamHandler):
+    def __init__(self, verbose=0, **kwargs):
+        super().__init__(verbose=verbose, **kwargs)
+
+
+class FileHandler(Handler, logging.FileHandler):
+    def __init__(self, filename, verbose=0, **kwargs):
+        super().__init__(filename=filename, verbose=verbose, **kwargs)
+
+
+class LogFile:
     """
-    Make a stream handler with consistent formatting
+    Context manager for file logging. It logs everything from `logger` in some file at a given `filename`.
+
+    Parameters
+    ----------
+    filename : str
+        Filename under which to save the log.
+    
+    verbose : int, optional
+        Logging level verbosity 0='CRITICAL' 1='WARNING' 2='INFO' 3='DEBUG'.
+    
+    loggername : str, optional
+        Name of the logger to send to file at `path`. Default is `'cpnest'` so 
+        all cpnest logs are recorded. E.g. specify `'cpnest.cpnest'` to only
+        record logs from the `cpnest.py` module.
+
+    Attributes
+    ----------
+    handler : logging.FileHandler
+        File handler object.
+
+    Examples
+    --------
+    ```python
+    from cpnest.utils import LogFile
+
+    with LogFile('example.log') as flog:
+        # Do some stuff here and it will be logged to 'example.log'
+        ...
+
+    # Do some stuff here and it won't be logged to 'example.log'
+
+    with flog:
+        # Do some stuff here and it will be logged to 'example.log'
+        ... 
+    ```
+
     """
-    sh = logging.StreamHandler()
-    sh.setFormatter(logging.Formatter(FMT, datefmt=DATE_FMT))
-    return sh
+
+    def __init__(self, filename, verbose=0, loggername='cpnest'):
+        self.filename = filename
+        self.verbose = verbose
+        self.logger = logging.getLogger(loggername)
+        self.handler = None
+
+    def __enter__(self):
+        self.handler = FileHandler(self.filename, verbose=self.verbose)
+        self.logger.addHandler(self.handler)
+        return self
+    
+    def __exit__(self, type, value, traceback):
+        self.logger.removeHandler(self.handler)
+        self.handler.close()  # Safely closes file handler
+        self.handler = None
