correct weight calculation for n threads and slice sample the likelihood bound prior

cpnest/NestedSampling.py
@@ -51,6 +51,8 @@ class _NSintegralState(object):
         """
         Increment the state of the evidence integrator
         Simply uses rectangle rule for initial estimate
+        see: https://www.cell.com/biophysj/pdf/S0006-3495(12)00055-0.pdf
+        for parallel implementation
         """
         if(logL<=self.logLs[-1]):
             self.logger.warning('NS integrator received non-monotonic logL. {0:.5f} -> {1:.5f}'.format(self.logLs[-1], logL))
@@ -210,13 +212,15 @@ class NestedSampler(object):
 
         l = []
 
-        for i in range(self.nlive):
+        with tqdm(total=self.nlive, disable = not self.verbose, desc='CPNEST: populate samplers', position=0) as pbar:
+            for i in range(self.nlive):
 
-            l.append(self.model.new_point())
-            l[-1].logP = self.model.log_prior(l[-1])
-            l[-1].logL = self.model.log_likelihood(l[-1])
+                l.append(self.model.new_point())
+                l[-1].logP = self.model.log_prior(l[-1])
+                l[-1].logL = self.model.log_likelihood(l[-1])
+                pbar.update()
 
-        self.live_points = LivePoints.remote(l)
+        self.live_points = LivePoints.remote(l, self.nthreads)
         self.live_points.update_mean_covariance.remote()
 
     def setup_output(self,output):
@@ -334,7 +338,7 @@ class NestedSampler(object):
             pool.submit(lambda a, v: a.produce_sample.remote(v, -np.inf), self.live_points.get.remote(i))
 
         i = 0
-        with tqdm(total=self.nlive, disable= not self.verbose, desc='CPNEST: populate samplers', position=self.nthreads) as pbar:
+        with tqdm(total=self.nlive, disable= not self.verbose, desc='CPNEST: sampling prior', position=self.nthreads) as pbar:
             while pool.has_next():
                 acceptance,sub_acceptance,self.jumps,x = pool.get_next()
                 if np.isnan(x.logL):
@@ -437,8 +441,8 @@ class NestedSampler(object):
 
 @ray.remote(num_cpus=1)
 class LivePoints:
-
-    def __init__(self, l, verbose = 2):
+    def __init__(self, l, n_replace = 1, verbose = 2):
+        self.n_replace            = n_replace
         self._list                = l
         self.n                    = len(l)
         self.dim                  = self._list[0].dimension
@@ -523,8 +527,11 @@ class LivePoints:
         self.worst = self._list[:n]
 
         for p in self.worst:
-            self.state.increment(p.logL)
+#            self.state.increment(p.logL)
             self.nested_samples.append(p)
+#        for p in self.worst:
+        self.state.increment(self.worst[n-1].logL, n=self.n_replace)
+        self.nested_samples.append(self.worst[n-1])
 
         return self.worst
 
@@ -538,7 +545,7 @@ class LivePoints:
         # final adjustments
         self._list.sort(key=attrgetter('logL'))
         for i,p in enumerate(self._list):
-            self.state.increment(p.logL,nlive=self.n-i)
+            self.state.increment(p.logL,nlive=self.n-i, n = 1)
             self.nested_samples.append(p)
 
         # Refine evidence estimate

cpnest/sampler.py
@@ -2,7 +2,6 @@ from __future__ import division
 import sys
 import os
 import logging
-import time
 import numpy as np
 from math import log
 from collections import deque
@@ -11,8 +10,6 @@ from random import random,randrange
 from . import parameter
 from .proposal import DefaultProposalCycle, EnsembleProposal
 from . import proposal
-from tqdm import tqdm
-from operator import attrgetter
 import numpy.lib.recfunctions as rfn
 import array
 from .nest2pos import acl
@@ -92,9 +89,8 @@ class Sampler(object):
         self.initialised        = False
         self.output             = output
         self.sample_prior       = sample_prior
-        self.max_storage        = 25000
+        self.max_storage        = 32768
         self.samples            = deque(maxlen = None if self.verbose >=3 else self.max_storage) # the list of samples from the mcmc chain
-        self.last_checkpoint_time = time.time()
 
     def estimate_nmcmc_on_the_fly(self, safety=5, tau=None):
         """
@@ -211,6 +207,27 @@ class HamiltonianMonteCarloSampler(Sampler):
     HamiltonianMonteCarlo acceptance rule
     for :obj:`cpnest.proposal.HamiltonianProposal`
     """
+    def estimate_nmcmc(self):
+        """
+        Estimate autocorrelation length of the chain
+        """
+        # first of all, build a numpy array out of
+        # the stored samples
+        ACL = []
+        s   = list(self.samples)
+        samples = np.array([x.values for x in s[-5*self.max_storage:]])
+        # compute the ACL on 5 times the maxmcmc set of samples
+        ACL = [acl(samples[:,i]) for i in range(samples.shape[1])]
+
+        if self.verbose >= 3:
+            for i in range(len(self.model.names)):
+                self.logger.info("Sampler {0} -- ACL({1})  = {2}".format(os.getpid(),self.model.names[i],ACL[i]))
+
+        self.Nmcmc = int(np.max(ACL))
+        if self.Nmcmc < 1:
+            self.Nmcmc = 1
+        return self.Nmcmc
+
     def yield_sample(self, oldparam, logLmin):
 
         while True:
@@ -232,7 +249,7 @@ class HamiltonianMonteCarloSampler(Sampler):
                 if sub_counter >= self.Nmcmc and sub_accepted > 0:
                     break
 
-                if sub_counter > self.maxmcmc:
+                if sub_counter >= self.maxmcmc:
                     break
 
             # append the sample to the array of samples
@@ -256,31 +273,9 @@ class SliceSampler(Sampler):
     https://arxiv.org/pdf/2002.06212v1.pdf
     """
     mu             = 1.0
-    max_steps_out  = 1000 # maximum stepping out steps allowed
-    max_slices     = 1000 # maximum number of slices allowed
+    max_steps_out  = 100 # maximum stepping out steps allowed
+    max_slices     = 100 # maximum number of slices allowed
     tuning_steps   = 1000
-
-    def estimate_nmcmc(self,):
-        """
-        Estimate autocorrelation length of the chain
-        """
-        # first of all, build a numpy array out of
-        # the stored samples
-        ACL = []
-        s   = list(self.samples)
-        samples = np.array([x.values for x in s[-5*self.maxmcmc:]])
-        # compute the ACL on 5 times the maxmcmc set of samples
-        ACL = [acl(samples[:,i]) for i in range(samples.shape[1])]
-
-        if self.verbose >= 3:
-            for i in range(len(self.model.names)):
-                self.logger.info("Sampler {0} -- ACL({1})  = {2}".format(os.getpid(),self.model.names[i],ACL[i]))
-
-        self.Nmcmc = int(np.max(ACL))
-        if self.Nmcmc < 1:
-            self.Nmcmc = 1
-        return self.Nmcmc
-
     def adapt_length_scale(self):
         """
         adapts the length scale of the expansion/contraction
@@ -399,7 +394,7 @@ class SliceSampler(Sampler):
                 if sub_counter >= self.Nmcmc and sub_accepted > 0:
                     break
 
-                if sub_counter > self.maxmcmc:
+                if sub_counter >= self.maxmcmc:
                     break
 
             self.samples.append(oldparam)

examples/test_50d.py
@@ -35,15 +35,15 @@ class GaussianTestCase(unittest.TestCase):
     Test the gaussian model
     """
     def setUp(self):
-        self.model=GaussianModel(dim = 50)
-        self.work=cpnest.CPNest(self.model, verbose=2, nthreads=10, nlive=1000, maxmcmc=5000, nslice=5, nhamiltonian=5, resume=1)
+        self.model=GaussianModel(dim = 20)
+        self.work=cpnest.CPNest(self.model, verbose=2, nthreads=1, nlive=1000, maxmcmc=100, nslice=1, nhamiltonian=0, resume=0)
 
     def test_run(self):
         self.work.run()
         # 2 sigma tolerance
         tolerance = 2.0*np.sqrt(self.work.NS.info/self.work.NS.nlive)
         self.assertTrue(np.abs(self.work.NS.logZ - self.model.analytic_log_Z)<tolerance, 'Incorrect evidence for normalised distribution: {0} instead of {1}'.format(self.work.NS.logZ ,self.model.analytic_log_Z))
-
+        print("analytic logZ = {0}".format(self.model.analytic_log_Z))
 def test_all():
     unittest.main(verbosity=2)
 