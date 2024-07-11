1faeee4ae6d99bd234a05fe2f2c3dc1a7201329b
John Veitch <john.veitch@ligo.org>
2024-07-11 12:06:49 +0100

    Drop support for python < 3.9

--- setup.py
+++ setup.py
@@ -62,9 +62,8 @@ setup(
             'Programming Language :: Python :: 3.12',
             'Programming Language :: Python :: 3.11',
             'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.7'],
+            'Programming Language :: Python :: 3.9'
+            ]
         keywords='nested sampling bayesian inference',
         packages=['cpnest'],
         install_requires=requirements,
