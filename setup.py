68b7bf329f7508b0036a6cf5ac81a15dee2a356d
John Veitch <john.veitch@ligo.org>
2025-10-12 13:40:43 +0100

    Update setup.py for python 3.14

--- setup.py
+++ setup.py
@@ -51,6 +51,8 @@ setup(
             'Development Status :: 4 - Beta',
             'License :: OSI Approved :: MIT License',
             'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.14',
+            'Programming Language :: Python :: 3.13',
             'Programming Language :: Python :: 3.12',
             'Programming Language :: Python :: 3.11',
             'Programming Language :: Python :: 3.10',
